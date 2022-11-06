c7dd8eca48ddc9ce26e7db9e86ca2e9967348d8c
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2022-11-06 22:50:11 +0300

    init push

---
+++ src/test/java/week3/ConnectionTest.java
@@ -0,0 +1,39 @@
+package week3;
+
+import java.sql.*;
+
+public class ConnectionTest {
+
+    public static void main(String[] args) throws SQLException {
+
+        String dbUrl = "jdbc:oracle:thin:@54.167.52.69:1521:XE";
+        String username = "hr";
+        String password = "hr";
+
+        Connection connection = DriverManager.getConnection(dbUrl,username,password);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * from Employees");
+
+        resultSet.next(); // this line will move my pointer to next line, which is 1st line of my table
+
+        System.out.println(resultSet.getString("first_name"));
+        System.out.println(resultSet.getString("last_name"));
+        System.out.println(resultSet.getString(4));
+        System.out.println(resultSet.getString(8));
+
+        resultSet.next();  // now my pointer is on the second line
+
+        System.out.println(resultSet.getString(5));
+
+        System.out.println("Full name is: " + resultSet.getString(2) + " "  + resultSet.getString(3));
+
+//        for (int i = 0; i < 105; i++) {
+//            System.out.println(resultSet.getString(2));
+//            resultSet.next();
+//        }
+
+        while (resultSet.next()){
+            System.out.println(resultSet.getString(2));
+        }
+    }
+}
