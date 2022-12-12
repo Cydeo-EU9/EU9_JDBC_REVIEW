8ca80f2fd0da6b86b34659a24b8ae12aa0e603f3
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2022-12-12 22:58:56 +0300

    review part is added

--- src/test/java/review/JDBCReview.java
+++ src/test/java/review/JDBCReview.java
@@ -8,5 +8,38 @@ public class JDBCReview {
 
     public static void main(String[] args) throws SQLException {
 
+        String db_url = "jdbc:oracle:thin:@3.87.65.105:1521:XE";
+        String db_username = "hr";
+        String db_password = "hr";
+
+        Connection connection = DriverManager.getConnection(db_url, db_username, db_password);
+        Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                ResultSet.CONCUR_READ_ONLY);
+        ResultSet resultSet = statement.executeQuery("select * from EMPLOYEES");
+
+        /*
+        when you want to get data from one row, your cursur should be on that row
+         */
+
+        resultSet.next();
+        System.out.println(resultSet.getString(1));
+        System.out.println(resultSet.getString(5));
+        System.out.println(resultSet.getString(7));
+        System.out.println(resultSet.getString("email"));
+
+
+
+        resultSet.next();
+        String firstName = resultSet.getString(2);
+        String lastName = resultSet.getString(3);
+
+
+        System.out.println("fullName = " + firstName + " " +  lastName);
+
+        while (resultSet.next()){
+            System.out.println(resultSet.getString("Job_id"));
+        }
+
+
     }
 }
